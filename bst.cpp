05916a297b2a623941d542c6929bf6ef4fb1a264
zubairmh <zubair.mh@protonmail.com>
2023-11-27 16:27:45 +0530

    bst implementation

---
+++ bst.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Node {
+  Node *left;
+  Node *right;
+  int val;
+};
+
+Node *CreateNode(int val) {
+  Node *tree = new Node;
+  tree->val = val;
+  return tree;
+}
+
+Node *Insert(Node *tree, int val) {
+  if (tree == NULL) {
+    return CreateNode(val);
+  }
+  if (val < tree->val) {
+    tree->left = Insert(tree->left, val);
+  } else if (val >= tree->val) {
+    tree->right = Insert(tree->right, val);
+  }
+
+  return tree;
+}
+
+void Inorder(Node *tree) {
+  if (tree == NULL) {
+    return;
+  }
+
+  if (tree->left) {
+    Inorder(tree->left);
+  }
+  cout << tree->val;
+  if (tree->right) {
+    Inorder(tree->right);
+  }
+}
+
+void Postorder(Node *tree) {
+  if (tree == NULL) {
+    return;
+  }
+
+  if (tree->left) {
+    Postorder(tree->left);
+  }
+  if (tree->right) {
+    Postorder(tree->right);
+  }
+  cout << tree->val;
+}
+
+void Preorder(Node *tree) {
+  if (tree == NULL) {
+    return;
+  }
+  cout << tree->val;
+  if (tree->left) {
+    Preorder(tree->left);
+  }
+  if (tree->right) {
+    Preorder(tree->right);
+  }
+}
+int main() {
+  Node *tree = CreateNode(6);
+  Insert(tree, 2);
+  Insert(tree, 4);
+  Insert(tree, 8);
+  Insert(tree, 9);
+  Insert(tree, 7);
+  Insert(tree, 5);
+  Inorder(tree);
+  cout << endl;
+  Postorder(tree);
+  cout << endl;
+  Preorder(tree);
+}
